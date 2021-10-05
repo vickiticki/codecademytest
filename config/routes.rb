618a06f40cae32e698b845702c867e3566df4e50
codecademy <ccuser@codecademy.com>
2021-10-05 16:12:01 +0000

    contact page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/portfolio' =>'pages#portfolio'
 
   get '/about' => 'pages#about'
+
+  get '/contact' => 'pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
